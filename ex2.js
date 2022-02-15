0c639ad881ca8361c4829f4430653de1e2a682bc
barmako <44745535+barmakoWS@users.noreply.github.com>
2022-02-15 10:11:06 +0200

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,11 @@
+var pgp = require('pg-promise')();
+
+
+function f1(a, q) {
+    return a(q) //f2()
+}
+
+function f2(t) {
+    var db = pgp("postgres://postgres:postgres@127.0.0.1/vulnerablenode");
+    return db.one(t);
+}
